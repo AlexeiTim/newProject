f06eba1e713fd08481581eb41d237f30d205fe99
AlexeiTim <timashkov060896@gmail.com>
2023-01-09 01:26:26 +0300

    Задачи на условия и циклы

--- app.js
+++ app.js
@@ -8,11 +8,56 @@ const personalMovieDB = {
   privat: false
 };
 
-let a = prompt('Один из последних просмотренных фильмов?', ''),
-  b = +prompt('На сколько оцените его?'),
-  c = prompt('Один из последних просмотренных фильмов?', ''),
-  d = +prompt('На сколько оцените его?');
+if (personalMovieDB.count < 10) {
+  console.log('мало');
+  alert('мало');
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 30) {
+  alert('норм');
+} else if (personalMovieDB.count > 30) {
+  alert('ВАУ');
+} else {
+  alert('Ошибка');
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// for (let i = 0; i < 2; i++) {
+//   let a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = +prompt('На сколько оцените его?');
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     console.log('done');
+//   } else {
+//     console.log('error');
+//     i--;
+//   }
+// }
+
+let num = 0;
+// while (num < 3) {
+//   num++;
+//   let a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = +prompt('На сколько оцените его?');
+//   if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//     personalMovieDB.movies[a] = b;
+//     num++;
+//     console.log('done');
+//   } else {
+//     console.log('error');
+//     num--;
+//   }
+// }
+
+do {
+  num++;
+  let a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = +prompt('На сколько оцените его?');
+  if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+    personalMovieDB.movies[a] = b;
+    num++;
+    console.log('done');
+  } else {
+    console.log('error');
+    num--;
+  }
+} while (num <= 2);
+
 console.log(personalMovieDB);
